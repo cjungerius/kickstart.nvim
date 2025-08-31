24cea44fb8ce90dc1e75b799bd67b540f3b4600d
Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
2025-08-31 20:33:12 +0200

    add lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,8 +2,16 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {},
     config = function()
-      require('lualine').setup()
+      require('lualine').setup {
+        options = {
+          disabled_filetypes = {
+            'snacks_dashboard',
+            'snacks_picker_list',
+          },
+        },
+      }
     end,
   },
 }
